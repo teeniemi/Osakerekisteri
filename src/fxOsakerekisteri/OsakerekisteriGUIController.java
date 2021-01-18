349dc96ca1dd79908432ebde8255b95a778abef0
Jepu <jesse.korolainen@hotmail.com>
2021-01-18 11:12:52 +0200

    Tehty pohja HT projektille.

---
+++ src/fxOsakerekisteri/OsakerekisteriGUIController.java
@@ -0,0 +1,10 @@
+package fxOsakerekisteri;
+
+/**
+ * @author jesse
+ * @version 18.1.2021
+ *
+ */
+public class OsakerekisteriGUIController {
+	//
+}
---
+++ src/fxOsakerekisteri/OsakerekisteriMain.java
@@ -0,0 +1,35 @@
+package fxOsakerekisteri;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * @author jesse
+ * @version 18.1.2021
+ *
+ */
+public class OsakerekisteriMain extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("OsakerekisteriGUIView.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @param args ei käytössä
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
