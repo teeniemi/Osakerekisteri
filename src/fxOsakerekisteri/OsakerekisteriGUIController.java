2b1aa79377700bdd1bd984a96d279905a9f93345
Teemu <teminiem@student.jyu.fi>
2021-01-25 20:16:12 +0200

    alkusäätöä koodin kera.

--- src/fxOsakerekisteri/OsakerekisteriGUIController.java
+++ src/fxOsakerekisteri/OsakerekisteriGUIController.java
@@ -1,10 +1,30 @@
 package fxOsakerekisteri;
 
+import fi.jyu.mit.fxgui.Dialogs;
+import javafx.fxml.FXML;
 /**
- * @author jesse
+ * @author Julio
  * @version 18.1.2021
  *
  */
 public class OsakerekisteriGUIController {
-	//
+	
+    @FXML
+    void handleUusiOsake() {
+    	Dialogs.showQuestionDialog("Uusi osake?", "Ostetaanko osake: Gamestop?", "Kyllä", "Eiku");
+
+    }
+	
+	
+    @FXML void handleOstaOsake() {
+    	osta();
+    }
+    
+    // ===============================================================
+    
+    
+    private void osta() {
+    	Dialogs.showMessageDialog("Ostit osakkeen! Mutta äläpä hättäile, ei toimi vielä.");
+    }
+	
 }
