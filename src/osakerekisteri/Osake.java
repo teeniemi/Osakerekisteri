0a4be7ca06d85b1f4116e1b9266c43a8b06b43ae
Teemu <teminiem@student.jyu.fi>
2021-03-01 15:16:23 +0200

    Osake-luokka alustavasti valmis.

--- src/osakerekisteri/Osake.java
+++ src/osakerekisteri/Osake.java
@@ -9,18 +9,18 @@ import java.io.PrintStream;
  * |-------------------------------------------------------------------------
  * | Vastuualueet:                                      |                   |
  * |                                                    |                   |
- * | - ei tiedï¿½ Osakerekisteristï¿½, osakkeista eikï¿½      |                   |
- * |   kï¿½yttï¿½liittymï¿½stï¿½                                |                   |
- * | - tietï¿½ï¿½ Osakkeen kentï¿½t (stockId, stockName,      |                   |
+ * | - ei tiedï¿½ Osakerekisteristï¿½, osakkeista eikï¿½|                   |
+ * |   kï¿½yttï¿½liittymï¿½stï¿½                        |                   |
+ * | - tietï¿½ï¿½ Osakkeen kentï¿½t (stockId, stockName,|                   |
  * |   amount, averagePrice, totalPrice)                |                   | 
- * | - osaa tarkistaa tietyn kentï¿½n                     |                   |
+ * | - osaa tarkistaa tietyn kentï¿½n                   |                   |
  * |   oikeellisuuden (syntaksin)                       |                   |
  * | - osaa muuttaa |Nordea Oyj|..| -                   |                   |
  * |   merkkijonon osakkeen tiedoiksi                   |                   |
- * | - osaa antaa merkkijonona i:n kentï¿½n               |                   |
+ * | - osaa antaa merkkijonona i:n kentï¿½n             |                   |
  * |   tiedot                                           |                   |
  * | - osaa laittaa merkkijonon i:neksi                 |                   |
- * |   kentï¿½ksi                                         |                   |
+ * |   kentï¿½ksi                                       |                   |
  * | - osaa muuttaa olion tiedoston riviksi             |                   |
  * |                                                    |                   |
  * |-------------------------------------------------------------------------
@@ -37,16 +37,18 @@ public class Osake {
 	private double 		averagePrice = 0;
 	private double 		totalPrice = 0;
 	
+	private static int nextId = 1;
+	
 	/**
 	 * Tulostetaan osakkeen tiedot
 	 * @param out tietovirta, johon tulostetaan
 	 */
-	public void tulosta(PrintStream out) {
+	public void print(PrintStream out) {
 		out.println("ID " + String.format("%03d", stockId));
 		out.println(" Name " + stockName);
 		out.println(" Amount " + amount);
-		out.println(" Average price " + String.format("%4.2f",averagePrice) + "ï¿½"); 
-		out.println(" Total price " + String.format("%4.2f", totalPrice) + "ï¿½");
+		out.println(" Average price " + String.format("%4.2f",averagePrice) + "€"); 
+		out.println(" Total price " + String.format("%4.2f", totalPrice) + "€");
 		out.println("--------------------------------------");
 		
 	}
@@ -56,12 +58,58 @@ public class Osake {
 	 * @param os tietovirta, johon tulostetaan
 	 */
 	
-	public void tulosta (OutputStream os) {
-		tulosta(new PrintStream(os));
+	public void print (OutputStream os) {
+		print(new PrintStream(os));
 	}
 	
 	/**
-	 * @param args ei kÃ¤ytÃ¶ssÃ¤
+	 * Rekisteröi osakkeelle id:n ja vierittää kiveä eteenpäin
+	 * @return osakeid
+	 * @example
+	 * <pre name="test">
+	 * Osake osake1 = new Osake();
+	 * osake1.getNextId() === 0;
+	 * osake1.register() === 1;
+	 * Osake osake2 = new Osake();
+	 * osake2.register();
+	 * int n1 = osake1.getNextId();
+	 * int n2 = osake2.getNextId();
+	 * n1 === n2-1;
+	 * </pre>
+	 */
+	
+	public int register() {
+		this.stockId = nextId;
+		nextId++;
+		return this.stockId;
+	}
+	
+	/**
+	 * Haetaan osakkeelle id
+	 * @return osakeid
+	 */
+	
+	public int getNextId() {
+		return stockId;
+	}
+	
+	/**
+	 * Haetaan osakkeen tiedot
+	 * @return osakkeen tiedot
+	 */
+	
+	
+	public void giveStock() {
+		stockId = 001;
+		stockName = "Nokia Oyj";
+		amount = 500;
+		averagePrice = 2.20;
+		totalPrice = 1100.00;
+	}
+	
+	
+	/**
+	 * @param args ei käytössä
 	 */
 	public static void main(String[] args) {
 		
@@ -69,16 +117,16 @@ public class Osake {
 		Osake osake = new Osake();
 		Osake osake2 = new Osake();
 		
-		// osake.rekisteroi();
-		// osake2.rekisteroi();
+		osake.register();
+		osake2.register();
 		
-		osake.tulosta(System.out);
-		// osake.vastaaOsake();
-		osake.tulosta(System.out);
+		osake.print(System.out);
+		osake.giveStock();
+		osake.print(System.out);
 		
-		osake2.tulosta(System.out);
-		// osake2.vastaaOsake();
-		osake.tulosta(System.out);
+		osake2.print(System.out);
+		osake2.giveStock();
+		osake2.print(System.out);
 	}
 
 }
